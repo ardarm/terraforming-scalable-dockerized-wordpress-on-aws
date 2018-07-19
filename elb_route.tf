dd581892ec0881cde145c83d00051050113b6ed3
aris.darmawan <arisrooney.09@gmail.com>
2018-07-20 01:07:03 +0700

    Create elb_route.tf

---
+++ elb_route.tf
@@ -0,0 +1,41 @@
+resource "aws_route53_zone" "wpdevops" {
+   name = "wp.devops" 
+   force_destroy = true
+}
+
+
+resource "aws_route53_record" "www_cc" {
+   zone_id = "${aws_route53_zone.wpdevops.zone_id}"
+   name = "www.wpdevops.tk"
+   type = "CNAME"
+   ttl = "300"
+   records = ["${aws_elb.wpdevops.dns_name}"]
+}
+
+resource "aws_route53_record" "apex" {
+  zone_id = "${aws_route53_zone.wpdevops.zone_id}"
+  name    = "wp.devops"
+  type    = "A"
+
+ alias {
+    name                   = "${aws_elb.wpdevops.dns_name}"
+    zone_id                = "${aws_elb.wpdevops.zone_id}"
+    evaluate_target_health = "false"
+  }
+}
+
+output "nameserver.1" {
+    value = "${aws_route53_zone.wpdevops.name_servers.0}"
+}
+
+output "nameserver.2" {
+    value = "${aws_route53_zone.wpdevops.name_servers.1}"
+}
+
+output "nameserver.3" {
+    value = "${aws_route53_zone.wpdevops.name_servers.2}"
+}
+
+output "nameserver.4" {
+    value = "${aws_route53_zone.wpdevops.name_servers.3}"
+}
